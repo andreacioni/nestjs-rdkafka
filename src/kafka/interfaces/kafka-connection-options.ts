c8f9179e9760554821efe7f445a1c37d13f8a4c7
Andrea Cioni <supercionci@hotmail.it>
2024-03-10 16:17:36 +0100

    add KafkaConnectionAsyncOptions; in KafkaConnectionOptions added global flag

--- src/kafka/interfaces/kafka-connection-options.ts
+++ src/kafka/interfaces/kafka-connection-options.ts
@@ -1,9 +1,20 @@
-import { KafkaAdminClientOptions } from './kafka-admin-client-options';
-import { KafkaConsumerOptions } from './kafka-consumer-options';
-import { KafkaProducerOptions } from './kafka-producer-options';
+import { ModuleMetadata } from "@nestjs/common";
+import { KafkaAdminClientOptions } from "./kafka-admin-client-options";
+import { KafkaConsumerOptions } from "./kafka-consumer-options";
+import { KafkaProducerOptions } from "./kafka-producer-options";
 
 export interface KafkaConnectionOptions {
-    consumer?: KafkaConsumerOptions,
-    producer?: KafkaProducerOptions
-    adminClient?: KafkaAdminClientOptions
+  consumer?: KafkaConsumerOptions;
+  producer?: KafkaProducerOptions;
+  adminClient?: KafkaAdminClientOptions;
+  global?: boolean;
+}
+
+export interface KafkaConnectionAsyncOptions
+  extends Pick<ModuleMetadata, "imports"> {
+  useFactory: (
+    ...args: any[]
+  ) => Promise<KafkaConnectionOptions> | KafkaConnectionOptions;
+  inject?: any[];
+  global?: boolean;
 }
